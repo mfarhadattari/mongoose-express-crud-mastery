2e9bf6bf10eb90906a0e4d92f53bbd09d878ab08
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-11-23 11:21:49 +0600

    implemented user model

---
+++ src/modules/user/user.model.ts
@@ -0,0 +1,95 @@
+import { Schema, model } from 'mongoose';
+import { IAddress, IName, IOrder, IUser } from './user.interface';
+
+// ------------------->> Creating Name Schema <<------------------- //
+const nameSchema = new Schema<IName>({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
+
+// ------------------->> Creating Address Schema <<------------------- //
+const addressSchema = new Schema<IAddress>({
+  street: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+});
+
+// ------------------->> Creating Order Schema <<------------------- //
+const orderSchema = new Schema<IOrder>({
+  productName: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
+// ------------------->> Creating User Schema <<------------------- //
+const userSchema = new Schema<IUser>({
+  userId: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  fullName: {
+    type: nameSchema,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  isActive: {
+    type: Boolean,
+    required: true,
+  },
+  hobbies: {
+    type: [String],
+    required: true,
+  },
+  address: {
+    type: addressSchema,
+    required: true,
+  },
+  orders: {
+    type: [orderSchema],
+    default: [],
+  },
+});
+
+export const UserModel = model('User', userSchema);
