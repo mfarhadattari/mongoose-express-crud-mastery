a2d15d28b488fca022af1f9cca8dc2fbcbced9b0
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-11-23 10:39:25 +0600

    created user interface

---
+++ src/modules/user/user.interface.ts
@@ -0,0 +1,27 @@
+export interface IAddress {
+  street: string;
+  city: string;
+  country: string;
+}
+
+export interface IOrder {
+  productName: string;
+  price: number;
+  quantity: number;
+}
+
+export interface IUser {
+  userId: number;
+  userName: string;
+  password: string;
+  fullName: {
+    firstName: string;
+    lastName: string;
+  };
+  age: number;
+  email: string;
+  isActive: boolean;
+  hobbies: string[];
+  address: IAddress;
+  orders: IOrder[];
+}
